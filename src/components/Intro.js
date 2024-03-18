fd9de0dab59e111f0733f26260dd1cd7715cc109
rafael.caldas <rafaelcroriz7@gmail.com>
2024-03-18 11:27:52 -0300

    Ajsute do layout dos botões sociais

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,32 +11,39 @@ const Intro = ({darkMode}) => {
                 <span className="introText">{t('introIm')}<span
                     className="introName">{t('introName')}</span><br/>{t('introRole')}</span>
                 <p className="introPara">{t('introDescription')}</p>
-                <div className="d-flex flex-row flex-wrap align-items-center mx">
-                    <a href="https://github.com/R4PTOR-C" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="https://img.shields.io/badge/-GitHub-%23181717?style=for-the-badge&logo=github&logoColor=white"
-                            alt="GitHub"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/rafael-caldas-76174b207" target="_blank"
-                       rel="noopener noreferrer">
-                        <img
-                            src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white"
-                            alt="LinkedIn"/>
-                    </a>
-                    <a href="https://instagram.com/rafaelroriz7" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white"
-                            alt="Instagram"/>
-                    </a>
-                    <a href="mailto:rafaelcroriz7@gmail.com" target="_blank" rel="noopener noreferrer">
-                        <img id="gmail-light"
-                             src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white"
-                             style={{display: darkMode ? 'none' : 'inline'}} alt="Gmail"/>
-                        <img id="gmail-dark"
-                             src="https://img.shields.io/badge/-Gmail-white?style=for-the-badge&logo=gmail&logoColor=%23333"
-                             style={{display: darkMode ? 'inline' : 'none'}} alt="Gmail Dark Mode"/>
-                    </a>
+                <div className="row justify-content-lg-start">
+                    <div className="col-6">
+                        <a href="https://github.com/R4PTOR-C" target="_blank" rel="noopener noreferrer"
+                           className="d-inline-block">
+                            <img
+                                src="https://img.shields.io/badge/-GitHub-%23181717?style=for-the-badge&logo=github&logoColor=white"
+                                alt="GitHub"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/rafael-caldas-76174b207" target="_blank"
+                           rel="noopener noreferrer" className="d-inline-block">
+                            <img
+                                src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white"
+                                alt="LinkedIn"/>
+                        </a>
+                        <a href="https://instagram.com/rafaelroriz7" target="_blank" rel="noopener noreferrer"
+                           className="d-inline-block">
+                            <img
+                                src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white"
+                                alt="Instagram"/>
+                        </a>
+                        <a href="mailto:rafaelcroriz7@gmail.com" target="_blank" rel="noopener noreferrer"
+                           className="d-inline-block">
+                            <img id="gmail-light"
+                                 src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white"
+                                 style={{display: darkMode ? 'none' : 'inline'}} alt="Gmail"/>
+                            <img id="gmail-dark"
+                                 src="https://img.shields.io/badge/-Gmail-white?style=for-the-badge&logo=gmail&logoColor=%23333"
+                                 style={{display: darkMode ? 'block' : 'none'}} alt="Gmail Dark Mode"/>
+                        </a>
+                    </div>
                 </div>
+
+
             </div>
 
         </div>
